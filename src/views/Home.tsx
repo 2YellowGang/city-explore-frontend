8d537319fa2e1a84651366d737d05f5d5f8c008e
Damian Cyntler <damian@cyntler.com>
2023-05-20 12:48:36 +0200

    feat: add home benefits

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,37 @@
 import { FC } from 'react';
+import { styled } from 'styled-components';
 import { ViewLayout } from '../components/ViewLayout';
+import { ContentContainer } from '../components/ContentContainer';
 
-export const Home: FC = () => <ViewLayout>home</ViewLayout>;
+const BenefitContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const BenefitItem = styled.div`
+  width: 330px;
+  height: 214px;
+  border-radius: 30px;
+  background-color: #dddddd;
+`;
+
+export const Home: FC = () => (
+  <ViewLayout>
+    <ContentContainer>
+      <h2>Zostań przewodnikiem</h2>
+      <p>
+        tekst motywujący Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat.
+      </p>
+      <BenefitContainer>
+        <BenefitItem></BenefitItem>
+        <BenefitItem></BenefitItem>
+        <BenefitItem></BenefitItem>
+      </BenefitContainer>
+      <h2>Znajdź miejsca</h2>
+    </ContentContainer>
+  </ViewLayout>
+);
