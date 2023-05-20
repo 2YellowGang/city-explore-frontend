69cb48062eef2f96c8f8fad7fc6bf579853ccc62
Konrad Gumienny <konrad.gumienny@gmail.com>
2023-05-21 00:00:06 +0200

    Added register view.

--- src/router.tsx
+++ src/router.tsx
@@ -4,9 +4,11 @@ import { Home } from './views/Home/Home';
 import { Guiders } from './views/Guiders';
 import { NotFound } from './views/NotFound/NotFound';
 import { Place } from './views/Place/Place';
+import { Register } from './views/Register/Register';
 
 export const router = createBrowserRouter([
   { path: '/', element: <Home />, errorElement: <NotFound /> },
   { path: '/place/:place/:locales', element: <Place /> },
   { path: '/guiders', element: <Guiders />, errorElement: <NotFound /> },
+  { path: '/register', element: <Register />, errorElement: <NotFound /> },
 ]);
---
+++ src/views/Register/Register.styles.tsx
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+import { Heading } from '../../components/Heading';
+
+export const RegisterContainer = styled.div`
+  display: flex;
+`;
+
+export const StyledHeading = styled(Heading)`
+  margin-top: 50px;
+`;
