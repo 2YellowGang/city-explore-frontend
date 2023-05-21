9ff6522a735b45a7e79d978b3dd8bdf6b61aa433
Damian Cyntler <damian@cyntler.com>
2023-05-21 08:35:05 +0200

    fix: list

--- src/views/Guiders/Guiders.tsx
+++ src/views/Guiders/Guiders.tsx
@@ -1,19 +1,20 @@
-import { FC, useCallback, useMemo } from 'react';
+import { FC, useCallback, useMemo, useState } from 'react';
 import { ViewLayout } from '../../components/ViewLayout';
 import { ContentContainer } from '../../components/ContentContainer';
 import { GuidersForm } from '../../components/GuidersForm';
 import { Heading } from '../../components/Heading';
-import { useParams } from 'react-router';
+// import { useParams } from 'react-router';
 import { _guiders } from '../../mockedData';
-import ReactStars from "react-rating-stars-component"
 import { Button, SecondButton } from '../../components/Button';
 import { GuidersContainer, GuidersItem } from './Guiders.styles';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
+import stars5 from '../../assets/stars5.svg';
 
 export const Guiders: FC = () => {
-  const { place, object } = useParams<{ place: string; object: string }>();
+  // const { place, object } = useParams<{ place: string; object: string }>();
+  const [sex, setSex] = useState('');
 
   const StyledHeading = styled(Heading)`
     margin-top: 50px;
@@ -25,68 +26,75 @@ export const Guiders: FC = () => {
   const handleGuidersNav = useCallback(
     (id: number) => {
       if (_guiders) {
-        navigate(
-          `/guider/${id}`,
-        );
+        navigate(`/guider/${id}`);
       }
     },
-    [navigate, _guiders],
+    [navigate],
   );
 
   const renderGuiders = useMemo(
     () =>
-      _guiders.map((obj) => (
-        <GuidersItem>
-          <div className='guider-container'>
-        <div className="guider-profile">
-            <img src={obj.avatar} />
-            <div>
-                <h3>{obj.name} {obj.surname}</h3>
-                <ReactStars
-                    count={5}
-                    onChange={() => {console.log()}}
-                    size={24}
-                    isHalf={true}
-                    emptyIcon={<i className="far fa-star"></i>}
-                    halfIcon={<i className="fa fa-star-half-alt"></i>}
-                    fullIcon={<i className="fa fa-star"></i>}
-                    activeColor="#26900B"
-                />
+      (sex ? _guiders.filter((guilder) => guilder.sex === sex) : _guiders).map(
+        (obj) => (
+          <GuidersItem>
+            <div className="guider-container">
+              <div className="guider-profile">
+                <img src={obj.avatar} />
+                <div>
+                  <h3 style={{ fontSize: '24px' }}>
+                    {obj.name} {obj.surname}
+                  </h3>
+                  <img
+                    src={stars5}
+                    style={{
+                      width: '110px',
+                      position: 'relative',
+                      left: '-5px',
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="guider-information">
+                <h5>Opis:</h5>
+                <p>{obj.description}</p>
+                <h5>Języki:</h5>
+                <p>{obj.languages.map((lang) => lang.name).join(', ')}</p>
+                <h5>Wiek:</h5>
+                <p>{obj.age}</p>
+              </div>
+              <div className="guider-actions">
+                <div>
+                  <SecondButton
+                    onClick={() => handleGuidersNav(obj.id)}
+                    style={{ fontSize: '14px', padding: 0 }}
+                  >
+                    Zobacz więcej
+                  </SecondButton>
+                  <SecondButton style={{ fontSize: '14px', padding: 0 }}>
+                    Skontaktuj się
+                  </SecondButton>
+                </div>
+                <div>
+                  <Button style={{ fontSize: '14px', padding: 0 }}>
+                    Zobacz w jakich miejscach przewodzę
+                  </Button>
+                </div>
+              </div>
             </div>
-        </div>
-        <div className="guider-information">
-            <h5>Opis:</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, velit!</p>
-            <h5>Języki:</h5>
-            <p>angielski, polski, ukraiński</p>
-        </div>
-        <div className="guider-actions">
-            <div>
-                <SecondButton onClick={() => handleGuidersNav(obj.id)}>Zobacz więcej</SecondButton>
-                <SecondButton>Skontaktuj się</SecondButton>
-            </div>
-            <div>
-                <Button>Zobacz w jakich miejscach przewodzę</Button>
-            </div>
-        </div>
-      </div>
-        </GuidersItem>
-      )),
-    [],
+          </GuidersItem>
+        ),
+      ),
+    [handleGuidersNav, sex],
   );
-
-
-  console.log(place, object);
 
   return (
     <ViewLayout>
-      <ContentContainer>
+      <ContentContainer style={{ marginBottom: '80px' }}>
         <StyledHeading>Dostępni przewoźnicy:</StyledHeading>
-        <GuidersForm />
-        <GuidersContainer>
-          {renderGuiders}
-        </GuidersContainer>
+        <GuidersForm sex={sex} onSexChange={setSex} />
+        <GuidersContainer>{renderGuiders}</GuidersContainer>
         <StyledHeading>Dostępni w innym terminie:</StyledHeading>
+        <GuidersContainer>{renderGuiders}</GuidersContainer>
       </ContentContainer>
     </ViewLayout>
   );
